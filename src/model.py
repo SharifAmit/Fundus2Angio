53acc31da3c834c426c821e28a0e72296e1390f2
SharifAmit <sharifamit@iub.edu.bd>
2020-05-17 21:12:18 -0700

    Fine generator and Discriminator

--- src/model.py
+++ src/model.py
@@ -43,8 +43,7 @@ def novel_residual_block(X_input, filters):
     return X
 
 
-def coarse_generator(img_shape=(256, 256, 3),ncf=64,
-                           n_downsampling=2, n_blocks=9, n_channels=1):
+def coarse_generator(img_shape=(256, 256, 3),ncf=64, n_downsampling=2, n_blocks=9, n_channels=1):
     X_input = Input(shape=img_shape)
     
 
@@ -83,4 +82,82 @@ def coarse_generator(img_shape=(256, 256, 3),ncf=64,
     model.compile(loss=['mse',None], optimizer=Adam(lr=0.0002, beta_1=0.5, beta_2=0.999))
 
     model.summary()
+    return model
+def fine_generator(x_coarse_shape=(256,256,64),input_shape=(512, 512, 3), nff=32, n_blocks=3, n_coarse_gen=1,n_channels = 1):
+
+    
+    X_input = Input(shape=input_shape)
+    X_coarse = Input(shape=x_coarse_shape)
+
+    for i in range(1, n_coarse_gen+1):
+        down_filters = nff * (2**(n_coarse_gen-i))
+        
+        # Downsampling layers
+        X = ReflectionPadding2D((3,3))(X_input)
+        X = Conv2D(down_filters, kernel_size=(7,7), strides=(1,1), padding='valid')(X)
+        X = BatchNormalization()(X)
+        x = LeakyReLU(alpha=0.2)(X)
+        X = Conv2D(down_filters*2, kernel_size=(3,3), strides=(2,2), padding='same')(X)
+        X = BatchNormalization()(X)
+        X = LeakyReLU(alpha=0.2)(X)
+
+        # Connection from Coarse Generator
+        
+        X = Add()([X_coarse,X])
+
+        for j in range(n_blocks):
+            res_filters = nff * (2**(n_coarse_gen-i)) * 2
+            X = novel_residual_block(X, res_filters)
+
+        # Upsampling layers
+        up_filters = nff * (2**(n_coarse_gen-i))
+        X = Conv2DTranspose(filters=up_filters, kernel_size=(3,3), strides=(2,2), padding='same')(X)
+        X = BatchNormalization()(X)
+        X = LeakyReLU(alpha=0.2)(X)
+
+    X = ReflectionPadding2D((3,3))(X)
+    X = Conv2D(n_channels, kernel_size=(7,7), strides=(1,1), padding='valid')(X)
+    X = Activation('tanh')(X)
+
+    model = Model(inputs=[X_input,X_coarse], outputs=X, name='G_Fine')
+    model.compile(loss='mse', optimizer=Adam(lr=0.0002, beta_1=0.5, beta_2=0.999))
+
+    model.summary()
+    return model
+def discriminator(input_shape_fundus=(512, 512, 3),
+                        input_shape_angio=(512, 512, 1),
+                        ndf=32, n_layers=3, activation='linear',
+                        n_downsampling=1, name='Discriminator'):
+    X_input_fundus = Input(shape=input_shape_fundus)
+    X_input_angio = Input(shape=input_shape_angio)
+
+
+    X = Concatenate(axis=-1)([X_input_fundus, X_input_angio])
+    for i in range(n_downsampling):
+        X = AveragePooling2D((3,3), strides=(2,2), padding='same')(X)
+
+    X = Conv2D(ndf, kernel_size=(4,4), strides=(2,2), padding='same',kernel_initializer=RandomNormal(stddev=0.02))(X)
+    X = LeakyReLU(alpha=0.2)(X)
+
+
+    for i in range(1, n_layers):
+        down_filters = min(ndf * 2, 512)
+        X = Conv2D(down_filters, kernel_size=(4,4), strides=(2,2), padding='same',kernel_initializer=RandomNormal(stddev=0.02))(X)
+        X = BatchNormalization()(X)
+        X = LeakyReLU(alpha=0.2)(X)
+
+
+    nf = min(nf * 2, 512)
+    X = Conv2D(nf, kernel_size=(4,4), strides=(1,1), padding='same',kernel_initializer=RandomNormal(stddev=0.02))(X)
+    X = BatchNormalization()(X)
+    X = LeakyReLU(alpha=0.2)(X)
+
+
+    X = Conv2D(1, kernel_size=(4,4), strides=(1,1), padding='same',kernel_initializer=RandomNormal(stddev=0.02))(X)
+    X = Activation(activation)(X)
+
+
+    model = Model(inputs=[X_input_fundus, X_input_angio], outputs=X , name=name)
+    model.summary()
+    model.compile(loss='mse', optimizer=Adam(lr=0.0002, beta_1=0.5, beta_2=0.999))
     return model
